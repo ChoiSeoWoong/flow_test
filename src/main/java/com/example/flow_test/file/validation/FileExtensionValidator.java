4d306b4fc5488510d9e6388f42e762297253ff93
Woongineering <chldbfkr456@naver.com>
2025-07-19 20:30:35 +0900

    feat : 파일 확장자 validation 추가

---
+++ src/main/java/com/example/flow_test/file/validation/FileExtensionValidator.java
@@ -0,0 +1,24 @@
+package com.example.flow_test.file.validation;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class FileExtensionValidator {
+    private final static int FILE_EXTENSION_NAME_MAX_LENGTH = 20;
+    private final static int CUSTOM_FILE_EXTENSION_LIST_MAX_SIZE = 200;
+    private final List<String> fileFixExtensionNameList = List.of("bat", "cmd", "com", "cpl", "exe", "scr", "js");
+
+    public boolean validateFileFixExtensionName(String fileExtensionName) {
+        return fileFixExtensionNameList.contains(fileExtensionName);
+    }
+
+    public boolean validateFileExtensionNameOfLength(String fileExtensionName) {
+        return fileExtensionName.length() <= FILE_EXTENSION_NAME_MAX_LENGTH;
+    }
+
+    public boolean validateCustomFileExtensionListMaxSize(int fileExtensionListSize) {
+        return fileExtensionListSize <= CUSTOM_FILE_EXTENSION_LIST_MAX_SIZE;
+    }
+}
