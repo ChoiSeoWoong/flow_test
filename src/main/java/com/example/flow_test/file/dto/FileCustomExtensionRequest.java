3cddfe0a87c9e0357931fb401cb1039e6ebca866
Woongineering <chldbfkr456@naver.com>
2025-07-19 20:31:51 +0900

    feat : 커스텀 파일 확장자 삽입 요청 dto 추가

---
+++ src/main/java/com/example/flow_test/file/dto/FileCustomExtensionRequest.java
@@ -0,0 +1,9 @@
+package com.example.flow_test.file.dto;
+
+import lombok.Getter;
+
+@Getter
+public class FileCustomExtensionRequest {
+    private String fileExtensionName;
+    private int fileExtensionNameListCount;
+}
